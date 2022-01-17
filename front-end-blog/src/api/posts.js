827006dc7f1c887ef66c45a0cbd281435a5692e1
Anshul Tamrakar <anshultamrakar487@gmail.com>
2022-01-17 14:14:11 +0530

    initial

--- front-end-blog/src/api/posts.js
+++ front-end-blog/src/api/posts.js
@@ -2,5 +2,7 @@ import axios from 'axios';
 
 
 export default axios.create({
-    baseURL : "http://localhost:3500"
+    // baseURL : "https://fake-server-blogapp.herokuapp.com"
+      baseURL : "http://localhost:5000"
+
 })
